bb4cea14f08a0a6972808e9fc5530dc4df9f570c
Ernest Perez <127113123+OpenSourceAIHardware@users.noreply.github.com>
2026-03-31 15:31:43 +1000

    Update output method in Main class
    Changed the output method to print the TreeMultiplier object.

--- Tree Multiplier in Java/Main.java
+++ Tree Multiplier in Java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //Test Example
         TreeMultiplier Test = new TreeMultiplier(true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
-        Test.MultiplierOutput();
+        System.out.println(Test);
     }
 }
